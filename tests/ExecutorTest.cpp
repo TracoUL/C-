31b132658c89d31301d03c1ae5e866a31942a172
TRACO\19935 <19935044754@163.com>
2024-10-15 21:00:47 +0800

    feat: Add test cases for turn right command

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -130,4 +130,48 @@ namespace adas
         const Pose target({0, 0, 'E'});
         ASSERT_EQ(target, executor->Query());
     }
+
+    // 测试用例11 测试测试Execute方法,在朝向为E,起点为(0,0)时去执行R指令是否正确
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_R_and_facing_is_E)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+        executor->Execute("R");
+
+        const Pose target({0, 0, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+
+    // 测试用例12 测试测试Execute方法,在朝向为S,起点为(0,0)时去执行R指令是否正确
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_R_and_facing_is_S)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+        executor->Execute("R");
+
+        const Pose target({0, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+
+    // 测试用例13 测试测试Execute方法,在朝向为W,起点为(0,0)时去执行R指令是否正确
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_R_and_facing_is_W)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+
+        executor->Execute("R");
+
+        const Pose target({0, 0, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+
+    // 测试用例14 测试测试Execute方法,在朝向为N,起点为(0,0)时去执行R指令是否正确
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_R_and_facing_is_N)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+        executor->Execute("R");
+
+        const Pose target({0, 0, 'E'});
+        ASSERT_EQ(target, executor->Query());
+    }
 }
