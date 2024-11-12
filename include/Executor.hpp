c3b96e4cdac9a9ca44281ddda9a12ebf3146b79e
TRACO\19935 <19935044754@163.com>
2024-11-12 21:06:53 +0800

    feat: Add test cases for Bus&SportCar

--- include/Executor.hpp
+++ include/Executor.hpp
@@ -10,13 +10,21 @@ namespace adas
         char heading; // 'N', 'S', 'E', 'W' 代表四个方向
     };
 
+//车辆类型
+    enum class ExecutorType
+    {
+        NORMAL,
+        SPORTS_CAR,
+        BUS,
+    };
+
     // 驾驶动作执行器接口
 
     class Executor
     {
     public:
         // Caller should delete *executor when it is no longer needed.
-        static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
+        static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}, const ExecutorType executorType = ExecutorType::NORMAL) noexcept;
 
     public:
         // 默认构造函数
